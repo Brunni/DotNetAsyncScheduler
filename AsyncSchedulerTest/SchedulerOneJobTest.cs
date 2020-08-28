ce9e890846f5f648f88c4d8b1cc9a7d3eee213a0
Michael Brunner <michael.brunner@xitaso.com>
2020-08-29 00:26:09 +0200

    #6 Fixed test timings and added .netcore2.1 support and fixed minver versioning
    * #6 Split test execution between net472, netcore2.0 and netcore3.1 for easier analysis
    
    * #6 Changed min-version to .net core 2.1
    
    * #6 Removed GitHubTestLogger for .netcoreapp2.1
    
    * #6 Attempt to fix netcoreapp2.1 test issues by downgrading .net.test.sdk
    
    * #6 Increased timings in test job
    
    * #6 Changed timing of test
    
    * #6 Attempt to setup .net core 2.1 as well
    
    * #6 Fixed github actions file
    
    * #6 Disabled netcore 2.1 tests on github
    
    * #6 Fixed minver versioning tags

--- AsyncSchedulerTest/SchedulerOneJobTest.cs
+++ AsyncSchedulerTest/SchedulerOneJobTest.cs
@@ -137,9 +137,9 @@ namespace AsyncSchedulerTest
             _scheduler.JobManager.AddJob<SimpleJob, ScheduleOnce>();
 
             // Act
-            var schedulerTask = RunScheduler(TimeSpan.FromSeconds(2));
+            var schedulerTask = RunScheduler(TimeSpan.FromSeconds(3));
             // Task is only executed once then we update schedule and task is triggered again
-            await Task.Delay(400);
+            await Task.Delay(1000);
             (_simpleJobInstance.ExecutionCount - executionCountBefore).Should().Be(1, $" executionCountBefore was {executionCountBefore}");
             await Task.Delay(100);
             _scheduler.JobManager.UpdateSchedule<SimpleJob, ScheduleEndless>();
@@ -155,7 +155,7 @@ namespace AsyncSchedulerTest
             lastSuccessfulJobResult?.JobResult.Should().Be(JobResult.Success);
             lastSuccessfulJobResult?.JobKey.Should().Be(jobKey);
 
-            (_simpleJobInstance.ExecutionCount - executionCountBefore).Should().BeInRange(3, 7);
+            (_simpleJobInstance.ExecutionCount - executionCountBefore).Should().BeInRange(5, 15);
         }
 
         private async Task RunScheduler(TimeSpan schedulerTime)
